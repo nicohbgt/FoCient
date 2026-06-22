b847ffd02475fdc3c024a456593f13fc1e523659
nicohbgt <nicohbgt512@gmail.com>
2026-06-22 14:07:00 +0700

    implement radius token

---
+++ app/lib/app/theme/app_radius.dart
@@ -0,0 +1,12 @@
+class AppRadius {
+  AppRadius._();
+
+  static const double r0 = 0;
+
+  static const double r4 = 4;
+  static const double r8 = 8;
+  static const double r12 = 12;
+  static const double r16 = 16;
+
+  static const double full = 9999;
+}
