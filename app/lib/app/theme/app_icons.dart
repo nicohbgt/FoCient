b4775c22536d85da00f0d6da664c3d5966115321
nicohbgt <nicohbgt512@gmail.com>
2026-06-22 13:56:16 +0700

    implement icons token

---
+++ app/lib/app/theme/app_icons.dart
@@ -0,0 +1,9 @@
+class AppIconSize {
+  AppIconSize._();
+
+  static const double xs = 16;
+  static const double sm = 20;
+  static const double md = 24;
+  static const double lg = 32;
+  static const double xl = 40;
+}
