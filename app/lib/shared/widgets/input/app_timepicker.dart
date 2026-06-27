aae80efe6c7fce0abdba067956887ec91ad3ffed
nicohbgt <nicohbgt512@gmail.com>
2026-06-27 16:23:33 +0700

    component-refactor 3

--- app/lib/shared/widgets/input/app_timepicker.dart
+++ app/lib/shared/widgets/input/app_timepicker.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import '../time picker/app_time_input.dart';
 import '../time picker/app_segment_button.dart';
 import '../time picker/app_am_pm_toggle.dart';
-import '../time picker/app_timezone_dropdown.dart';
+import '../input/app_dropdown.dart';
 
 class AppTimePicker extends StatelessWidget {
   const AppTimePicker({
@@ -101,9 +101,39 @@ class AppTimePicker extends StatelessWidget {
               ],
             ),
             const SizedBox(height: 20),
-            AppTimezoneDropdown(
+            AppDropdown<String>(
+              label: 'Timezone',
+              hint: 'Select timezone',
+              items: const [
+                'UTC-12:00',
+                'UTC-11:00',
+                'UTC-10:00',
+                'UTC-09:00',
+                'UTC-08:00',
+                'UTC-07:00',
+                'UTC-06:00',
+                'UTC-05:00',
+                'UTC-04:00',
+                'UTC-03:00',
+                'UTC-02:00',
+                'UTC-01:00',
+                'UTC+00:00',
+                'UTC+01:00',
+                'UTC+02:00',
+                'UTC+03:00',
+                'UTC+04:00',
+                'UTC+05:00',
+                'UTC+06:00',
+                'UTC+07:00',
+                'UTC+08:00',
+                'UTC+09:00',
+                'UTC+10:00',
+                'UTC+11:00',
+                'UTC+12:00',
+              ],
               value: timezone,
-              onChanged: onTimezoneChanged,
+              itemLabelBuilder: (value) => value,
+              onSelected: (value) => onTimezoneChanged(value),
             ),
             const SizedBox(height: 20),
             Row(
